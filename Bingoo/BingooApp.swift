dbe06721cadac99302a3516d340a1c12da32e53a
darkCavalier11 <sumit.kumar@changepay.in>
2024-04-27 12:24:01 +0530

    Initial Commit

---
+++ Bingoo/BingooApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingooApp.swift
+//  Bingoo
+//
+//  Created by Sumit Pradhan on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BingooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
