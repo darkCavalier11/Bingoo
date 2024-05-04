4d2080b47d71c771d8f860b888c76bac8dbb6dc3
darkCavalier11 <sumit.kumar@changepay.in>
2024-05-04 22:50:58 +0530

    added line crossing logic

--- Bingoo/ContentView.swift
+++ Bingoo/ContentView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     private let gridFrame = CGSize(width: GridTile.itemSize.height * 5, height: GridTile.itemSize.width * 5)
+    
+    @State private var crossLineFrameWidths = Array(repeating: 0.0, count: 12)
+    @State private var crossLineFrameHeights = Array(repeating: 0.0, count: 12)
+    
     @Environment(BingoState.self) var bingoState
     
     let bingo: [Character] = ["B","I","N","G","O"]
@@ -35,6 +39,33 @@ struct ContentView: View {
                     }
                         .offset(positionElement(row: element.row, column: element.column))
                 }
+                
+                /// Adding initial line for all rows, columns and diagonals and later
+                /// animate it to fill the group.
+                ///
+                /// 1. Adding row cross lines
+                ForEach(-2..<3) { idx in
+                    Rectangle()
+                        .frame(width: crossLineFrameWidths[idx + 2], height: crossLineFrameHeights[idx + 2])
+                        .offset(CGSize(width: 0, height: Double(idx) * GridTile.itemSize.height))
+                }
+                /// 2. Adding column cross lines
+                ForEach(-2..<3) { idx in
+                    Rectangle()
+                        .frame(width: crossLineFrameWidths[idx + 7], height: crossLineFrameHeights[idx + 7])
+                        .offset(CGSize(width: 0, height: -Double(idx) * GridTile.itemSize.height))
+                        .rotationEffect(Angle(degrees: 90))
+                }
+                
+                /// 3. Adding diagonal cross lines
+                /// a. Top left to bottom to bottom right
+                Rectangle()
+                    .frame(width: crossLineFrameWidths[10], height: crossLineFrameHeights[10])
+                    .rotationEffect(Angle(degrees: 45))
+                /// b. bottom left to top right
+                Rectangle()
+                    .frame(width: crossLineFrameWidths[11], height: crossLineFrameHeights[11])
+                    .rotationEffect(Angle(degrees: -45))
             }
             .onAppear {
                 bingoState.generateRandomGridTileElements()
@@ -52,12 +83,27 @@ struct ContentView: View {
             .background(.accent)
             .cornerRadius(10)
             .offset(x: gridFrame.width / 2 - 15)
+            
+            
         }
         .onChange(of: bingoState.completedGridGroups) { prev, curr in
-            print(prev)
-            print(curr)
+            if curr.isEmpty {
+                return
+            }
+            switch curr.last {
+            case .row(let rowIndex):
+                markRow(rowIndex)
+            case .column(let colIndex):
+                markColumn(colIndex)
+            case .diagonal(let diagonalType):
+                markDiagonal(diagonalType)
+            default:
+                print("")
+            }
         }
     }
+    
+
     
     func positionGridHeaderText(index: Int) -> CGSize {
         let xOffset = Double(index) * GridHeaderText.itemSize.width
@@ -66,8 +112,39 @@ struct ContentView: View {
     func positionElement(row: Int, column: Int) -> CGSize {
         let xOffset = Double(row) * GridTile.itemSize.width
         let yOffset = Double(column) * GridTile.itemSize.height
+        let width = yOffset - gridFrame.width / 2 + GridTile.itemSize.width / 2
+        let height =  xOffset - gridFrame.height / 2 + GridTile.itemSize.height / 2
+        return CGSize(width: width, height: height)
+    }
+    
+    func markRow(_ rowIndex: Int) {
+        crossLineFrameHeights[rowIndex] = 4
+        withAnimation {
+            crossLineFrameWidths[rowIndex] = 550
+        }
+    }
+    
+    func markColumn(_ colIndex: Int) {
+        crossLineFrameHeights[colIndex + 5] = 4
+        withAnimation {
+            crossLineFrameWidths[colIndex + 5] = 550
+        }
         
-        return CGSize(width: yOffset - gridFrame.width / 2 + GridTile.itemSize.width / 2, height: xOffset - gridFrame.height / 2 + GridTile.itemSize.height / 2)
+    }
+    
+    func markDiagonal(_ diagonalType: BingoState.CompletedGridType.DiagonalDirection) {
+        if diagonalType == .topLeftToBottomRight {
+            crossLineFrameHeights[10] = 4
+            withAnimation {
+                crossLineFrameWidths[10] = 750
+            }
+            
+        } else {
+            crossLineFrameHeights[11] = 4
+            withAnimation {
+                crossLineFrameWidths[11] = 750
+            }
+        }
     }
 }
 
