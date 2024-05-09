0dc13124fdc12aea522068d877b03b73363ee44f
darkCavalier11 <sumit.kumar@changepay.in>
2024-05-09 17:52:22 +0530

    revert changes

---
+++ Bingoo/Screens/GridTileGroupCrossViews.swift
@@ -0,0 +1,18 @@
+//
+//  GridTileGroupCrossViews.swift
+//  Bingoo
+//
+//  Created by Sumit Pradhan on 09/05/24.
+//
+
+import SwiftUI
+
+struct GridTileGroupCrossViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridTileGroupCrossViews()
+}
